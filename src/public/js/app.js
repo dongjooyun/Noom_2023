3640ddcf2e6762dfb9d9341a4bcf01e7f51dcf76
djyun <dongjooyun@khu.ac.kr>
2023-07-08 13:49:53 +0900

    added #2 Room Message code

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,16 +2,36 @@ const socket = io();
 
 const welcome = document.querySelector("#welcome");
 const form = welcome.querySelector("form");
+const room = document.querySelector("#room");
 
-function backendDone(msg) {
-  console.log(`The backend says: `, msg);
+let roomName;
+
+room.hidden = true;
+
+function showRoom(msg) {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `${roomName}방`;
+}
+
+function addMessage(msg) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = msg;
+  ul.appendChild(li);
 }
 
 function handleRoomSubmit(e) {
   e.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, backendDone);
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+  addMessage("Someone just joined.");
+});
